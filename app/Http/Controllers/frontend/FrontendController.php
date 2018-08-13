507aad0b99feffa213de0277a11a9d362bf64072
ymendoza <yoe318@gmail.com>
2018-08-12 22:11:32 -0400

    ajuste finales para geolocalizacion

--- app/Http/Controllers/frontend/FrontendController.php
+++ app/Http/Controllers/frontend/FrontendController.php
@@ -17,18 +17,32 @@ class FrontendController extends Controller
     	//$paises = Paises::where('pais_active', 1)->get();
 
     	//return view('frontend.index')->with('paises',$paises);
-     if (empty($_POST['checkip']))
-       {
-        $ip = $_SERVER["REMOTE_ADDR"]; 
-       }
-       else
-       {
-         $ip = $_POST['checkip']; 
-       }
-        
-        $pais =getCountryFromIP($ip, " NamE");
-        $code = getCountryFromIP($ip, "code");        
-        return view('frontend.index');
+
+      $dip = $_SERVER['REMOTE_ADDR'];
+      $TOKEN = "2fa61aa67d6a6f";
+      $city="";
+      $region="";
+      $country="";
+      $loc="";
+      $org="";
+      $json = file_get_contents("https://ipinfo.io??token=$TOKEN".$dip);
+      $details = json_decode($json,true);
+      if(array_key_exists("ip",$details)) $dip.=$details["ip"];  
+      if(array_key_exists("city",$details)) $city.=$details["city"];
+      if(array_key_exists("region",$details)) $region.=$details["region"];
+      if(array_key_exists("country",$details)) $country.=$details["country"];
+      if(array_key_exists("loc",$details)) $loc.=$details["loc"];
+      if(array_key_exists("org",$details)) $org.=$details["org"];
+      if($country=='AR' || $country=='BO' || $country=='CL' || $country=='BR' || $country=='CO' || $country=='CR' || $country=='CU' || $country=='DO' || $country=='EC' || $country=='GT' || $country=='CO' || $country=='HN' || $country=='PA' || $country=='PE' || $country=='PR' || $country=='PY' || $country=='SV' || $country=='UY' || $country=='VE' || $country=='ES'){
+          $locale = 'es';
+         
+      }else{
+         $locale = 'en';
+      }
+       \Session::put('locale', $locale);
+           //return redirect()->back();
+       
+      return view('frontend.index')->with('pais',$country)->with('region',$region)->with('ciudad',$city);
 
     }
 

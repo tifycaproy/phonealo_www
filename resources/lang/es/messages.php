83581f187dae60979221ee7e547e8269532499be
yosecc <yosec.cervino@gmail.com>
2018-08-18 23:44:53 -0400

    FrontendV1.11

--- resources/lang/es/messages.php
+++ resources/lang/es/messages.php
@@ -49,6 +49,135 @@ return[
   'menuItem1' => 'Tu Phonealo',
   'menuItem2' => 'Tu Amigo',
   'mas' => 'Ver más',
+  'privacity' => ' <h2>Política de privacidad</h2>
+   <p>Nosotros, Apel Global GSMA, S.R.L., con domicilio en B-Duc Mircea Cel Batran H5, Targoviste, Romania, junto con nuestras filiales, nos comprometemos a proteger tu información personal y respetar tu privacidad. En esta Política de privacidad describimos cómo recopilamos y tratamos tus datos personales. La Política de privacidad también establece los derechos que tienes con respecto a nosotros y cómo ejercerlos.</p> 
+
+   <p>Si tienes alguna pregunta sobre esta Política de privacidad o sobre tus datos personales en general ponte en contacto con nosotros en contact@phonealo.com.</p>
+   <p>ESTA POLÍTICA DE PRIVACIDAD ES DE INTERÉS PARA CUALQUIER PERSONA QUE VISITE NUESTRA PÁGINA WEB, UTILICE NUESTROS SERVICIOS Y PRODUCTOS O QUE DE ALGÚN MODO INTERACTÚE CON NOSOTROS. ES IMPORTANTE QUE LEAS Y COMPRENDAS NUESTRA POLÍTICA DE PRIVACIDAD ANTES DE UTILIZAR NUESTROS SERVICIOS Y PRODUCTOS.</p>
+   <h3>1. ¿Qué información sobre ti recopilamos, cómo la usamos y por qué es legal hacerlo?
+Tenemos varias maneras de recopilar información personal. Por ejemplo, cuando visitas nuestra página web, te descargas nuestra aplicación, interactúas con anuncios electrónicos, utilizas nuestros servicios, realizas una compra, te pones en contacto con nosotros o utilizas nuestro portal de clientes.</h3>
+
+   <p>Categorías de datos personales que recopilamos y tratamos:</p>
+    
+    <ul>
+      <li>Registro e información de la cuenta: p. ej.: tu número de teléfono, nombre, dirección de correo electrónico, foto de perfil, dirección postal, contraseña, huso horario, idioma, divisa o número de IVA intracomunitario.</li>
+      <br>
+      <li>Información sobre tu dispositivo y conexión: Recopilamos información relativa a los dispositivos y la conexión cuando instalas, accedes y utilizas nuestros servicios. Esto incluye información sobre el modelo del equipo y el sistema operativo; la versión de la aplicación; información sobre el navegador y red móvil; datos sobre la conexión, como, por ejemplo, el número de teléfono, el operador de teléfono o móvil y el proveedor de servicios de Internet; idioma y huso horario, IP; información sobre operaciones del dispositivo e identificadores, tales como identificadores de dispositivo. Cuando instales la aplicación de Phonealo te pediremos que nos des permiso para acceder a la agenda de tu teléfono móvil. Para facilitar la funcionalidad y gestionar nuestros servicios, guardaremos una copia de los números de teléfono, nombres y, si procede, identificadores de cuenta de los servicios Nauta, (solo recopilaremos el nombre, número de teléfono e identificador de la cuenta Nauta, independientemente de que sean o no usuarios de Phonealo).</li>
+      <br>
+      <li>Actividad e información de uso: p. ej.: información sobre el uso que haces de nuestros servicios y otras actividades relativas a Phonealo: información sobre cuándo te registraste para utilizar nuestros servicios; cómo utilizas nuestros servicios; artículos que compras; funciones que utilizas; la configuración de los servicios; el tiempo, la frecuencia y la duración de tus actividades e interacciones (p. ej.: números de teléfono y otros datos de identificación tanto del emisor como del receptor; hora de inicio y duración de la llamada; mensajes, recargas móviles monetarias y avisos de entrega); archivos de registro; archivos e informes sobre diagnósticos, fallos, página web y rendimiento.</li>
+      <br>
+      <li>Información sobre localización: Recopilamos información sobre la localización de dispositivos si utilizas nuestras funciones de localización.</li>
+      <br>
+      <li>Atención al cliente y redes sociales: Si utilizas nuestro servicio de Atención al cliente, interactúas con nosotros en nuestras redes sociales o de cualquier otra manera, nosotros podremos registrar dicha interacción, inclusive la correspondencia tal como copias de correos electrónicos y tu dirección de correo electrónico.</li>
+      <br>
+      <li>Datos sobre conducta y rastreo: p. ej.: información sobre localización, patrones de conducta, preferencias personales, número de IP, identificadores de cookies, identificadores únicos de dispositivos que utilizas para acceder y utilizar nuestros servicios y página web.</li>
+      <br>
+      <li>Información sobre pagos: Recopilamos datos de facturación, nombre, dirección postal, dirección de correo electrónico, datos bancarios y otra información financiera correspondiente al método de pago que hayas seleccionado. No obstante, solo guardaremos el nombre, la dirección postal, la dirección electrónica y el banco emisor; es decir, no guardaremos los datos de la tarjeta de crédito.</li>
+
+    </ul>
+    <p>Utilizaremos los datos que hayamos recopilado de conformidad con la lista que se indica más adelante. En resumen, utilizamos estos datos para prestar, ejecutar y mejorar nuestros servicios. La base jurídica para el tratamiento de datos se especifica a continuación.</p>
+<div class="table-responsive">
+    <table class="table">
+      <thead>
+        <tr>
+          <th>¿Cómo lo utilizamos? (objetivo del tratamiento de datos)</th>
+          <th>Base jurídica del tratamiento</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>Para confirmar tu identidad y verificar tus datos personales y de contacto; prestar y comercializar nuestros servicios; cumplir con los acuerdos relevantes suscritos contigo; y gestionar nuestra relación empresarial.</td>
+          <td>Cumplir nuestras obligaciones contractuales contigo, cumplir con la legislación vigente y perseguir los intereses legítimos de Phonealo.</td>
+        </tr>
+        <tr>
+          <td>Realizar el pago de servicios.</td>
+          <td>Cumplir nuestras obligaciones contractuales contigo, cumplir con la legislación vigente y perseguir los intereses legítimos de Phonealo.</td>
+        </tr>
+        <tr>
+          <td>Realizar análisis de marketing, producto y clientes para mejorar nuestros servicios y optimizar nuestra oferta a los clientes.</td>
+          <td>Cumplir nuestras obligaciones contractuales contigo, cumplir con la legislación vigente y perseguir los intereses legítimos de Phonealo.</td>
+        </tr>
+        <tr>
+          <td>Realizar análisis de clientes y de producto para operaciones internas, inclusive la resolución de problemas, análisis de datos, verificaciones e investigaciones, así como para fines estadísticos.</td>
+          <td>Cumplir nuestras obligaciones contractuales contigo y perseguir los intereses legítimos de Phonealo.</td>
+        </tr>
+        <tr>
+          <td>Llevar a cabo análisis de riesgo, detección de fraude y gestión del riesgo para prevenir el uso indebido de los servicios con el fin de garantizar la seguridad de los mismos.</td>
+          <td>Perseguir los intereses legítimos de Phonealo.</td>
+        </tr>
+        <tr>
+          <td>Para mejorar nuestros servicios y para fines generales de desarrollo empresarial, como el desarrollo de nuevos productos y funciones, y también para explorar nuevas oportunidades de negocio.</td>
+          <td>Perseguir los intereses legítimos de Phonealo.</td>
+        </tr>
+        <tr>
+          <td>Para cumplir con la legislación y normativas vigentes (por ejemplo, contabilidad, interceptación legal).</td>
+          <td>Cumplir con la legislación vigente y perseguir los intereses legítimos de Phonealo.</td>
+        </tr>
+        <tr>
+          <td>Para comunicarnos contigo en relación con nuestros servicios, así como para proporcionar atención al cliente.</td>
+          <td>Cumplir nuestras obligaciones contractuales contigo y perseguir los intereses legítimos de Phonealo.</td>
+        </tr>
+        <tr>
+          <td>Para establecer, ejercer o defender una demanda legal o proceso de cobro de pagos.</td>
+          <td>Cumplir nuestras obligaciones contractuales contigo, cumplir con la legislación vigente y perseguir los intereses legítimos de Phonealo.</td>
+        </tr>
+      </tbody>
+    </table>
+    </div>
+
+    <h3>2. Medidas de seguridad para datos personales</h3>
+   <p>Procesamos tus datos personales en virtud de la legislación y normativa vigente en materia de protección de datos. Hemos adoptado las medidas de seguridad jurídicas, técnicas y organizacionales correspondientes para proteger tus datos personales de divulgación o acceso no autorizado, uso indebido, pérdida, alteración o destrucción. Las medidas de seguridad que hemos adoptado se han creado para garantizar un nivel de seguridad adecuado a los riesgos que se asocian con las actividades de procesamiento de datos que realizamos. Si tienes alguna pregunta en relación con nuestras medidas de seguridad ponte en contacto con nosotros en contact@Phonealo.com.</p>
+
+   <h3>3. Información compartida</h3>
+   <p>Trabajamos con proveedores de servicios externos (o grupos de empresas) que han sido cuidadosamente seleccionados para operar, prestar, mejorar, comprender, personalizar, apoyar y comercializar nuestros servicios. Por ejemplo, trabajamos con empresas que distribuyen nuestras aplicaciones, proporcionan infraestructura, realizan entregas, proporcionan ubicación, previenen fraude u otras conductas delictivas, almacenan datos, procesan pagos, nos ayudan a comprender cómo se utilizan nuestros servicios, comercializan nuestros servicios y ofrecen atención al cliente. En relación con lo mencionado en anteriores párrafos, podremos compartir datos personales con estos proveedores de servicios y, en algunos casos, recibir datos personales de ellos.</p>
+    <p>Asimismo podremos compartir tus datos personales en las siguientes situaciones: si así lo requieren las autoridades (por ejemplo, la policía y las autoridades fiscales), podremos proporcionarles datos personales si estamos obligados a ello por ley; para cumplir o aplicar nuestras Condiciones de servicio u otro acuerdo que hayamos suscrito contigo; para proteger los derechos, la propiedad o la seguridad nuestra, de nuestros usuarios o de otros; y, finalmente, en caso de la venta de activos de Phonealo o un grupo de empresas o reestructuración corporativa, o como resultado de un cambio de control de Phonealo o de un grupo de empresas.</p>
+  <p>  Al compartir tus datos nos esforzamos por garantizar que las terceras partes relevantes apliquen las medidas organizacionales, técnicas y jurídicas adecuadas para proteger y salvaguardar los datos personales de acuerdo con la legislación vigente de protección de datos, la normativa aplicable de las autoridades de supervisión relevante y las directrices sobre seguridad de datos personales y, en general, aquello que sea relevante.</p>
+  <p>Prometemos no vender nunca tus datos personales a terceros a menos que nos des permiso para ello de forma explícita.</p>
+<br>
+  <p><b>Traspaso de datos a terceros</b></p>
+  <p>Preferimos tratar tus datos dentro de la UE/EEE. Sin embargo, en algunos casos traspasamos datos fuera de la UE/EEE. Antes de traspasar datos fuera de la UE/EEE tomamos medidas razonables para garantizar lo siguiente:</p>
+
+  <ul>
+    <li>Que las terceras partes aplican un nivel adecuado de protección de datos personales;</li>
+    <li>Que existen las garantías adecuadas en virtud de la legislación vigente sobre protección de datos, como las cláusulas contractuales estándar de la Comisión Europea;</li>
+    <li>Que podemos acogernos a una excepción específica en virtud de la legislación de protección de datos.</li>
+  </ul>
+
+<h3>Comunicaciones y notificaciones</h3>
+   <p>Phonealo podrá utilizar tus datos personales para comunicarse contigo. La comunicación podrá incluir mensajes sobre actualizaciones, errores, alertas y cualquier otro asunto relativo a los aspectos técnicos y la facilidad de uso de la aplicación y otras partes de los servicios. Dicha comunicación se considera parte del servicio.</p>
+   <p>En alguna ocasión podremos pedirle que nos facilite información acerca de su experiencia utilizando la aplicación u otras partes de los servicios que se utilizarán para mejorar la calidad y la prestación de servicios. No tiene la obligación de proporcionar tal información. Dicha información se utilizará para revisar la mejora de nuestros servicios.</p>
+   <p>Phonealo puede, en ocasiones, contactar con usted por teléfono, SMS o mediante cualquier otro canal informativo apropiado para mantenerlo informado sobre las últimas noticias de Phonealo y sus servicios, inclusive nuevos productos, campañas, promociones y métodos de pago.</p>
+   <p>Si no desea recibir mensajes promocionales de Phonealo podrá solicitar darse de baja siguiendo las instrucciones del mensaje promocional que haya recibido o poniéndose en contacto con nuestro equipo de atención al cliente en contact@Phonealo.com.</p>
+
+<p><b>SMS promocionales a clientes de los Estados Unidos.</b></p>
+<p>En ocasiones podremos enviar SMS promocionales a clientes que residen en los EE. UU. Para cancelar estos mensajes envía STOP al número desde el que ha recibido el SMS. Si deseas unirte de nuevo, envíanos un correo electrónico a contact@Phonealo.com y empezaremos a enviarte SMS promocionales de nuevo. Como siempre, podrán aplicarse cargos por los mensajes y datos en cualquier mensaje que te envíe Phonealo y que tú envíes a Phonealo.</p>
+
+<h3>5. Sitios web de terceros</h3>
+<p>Nuestro sitio web podrá contener enlaces a páginas web de terceros. Ten en cuenta que nosotros no controlamos los sitios web de los enlaces. Si quieres saber más sobre cómo estas terceras partes tratan tus datos personales deberás leer las políticas de privacidad de las mismas. Nuestra política de privacidad se aplica únicamente a los datos personales que recibimos o recopilamos a través de nuestra página web, nuestra aplicación, el uso que hagas de nuestros servicios y tu relación con Phonealo.</p>
+
+<h3>6. Cambios en la política de privacidad</h3>
+
+<p>Estamos constantemente innovando y buscando nuevas formas de mejorar nuestros servicios. Por lo tanto nos reservamos el derecho de actualizar y modificar esta política de privacidad en cualquier momento y sin previo aviso para reflejar dichas mejoras o cualquier otro cambio en nuestro negocio, así como para cumplir con la legislación vigente de protección de datos.</p>
+
+<p>Nuestra política de privacidad está siempre disponible para consulta en nuestra página web (en el apartado de Información legal) y también en nuestra aplicación (en Atención al cliente) de forma que los usuarios pueden, en todo momento, saber qué tipo de datos personales recopilamos, cómo y por qué se procesan los datos y bajo qué circunstancias dicha información podrá ser compartida. Te recomendamos que revises la política de privacidad de forma periódica para estar al corriente de cualquier modificación. Si no estás de acuerdo con las condiciones de la política no deberías utilizar los servicios.</p>
+
+<h3>7. Retención de datos</h3>
+<p>Los datos personales recopilados se guardarán durante todo el tiempo que sea necesario para nosotros (hasta un máximo de dos años y 10 meses a partir de que hayas cerrado la cuenta) a menos que estemos legalmente obligados a mantener los datos en nuestros registros durante un periodo de tiempo más prolongado. Cuando Phonealo guarda datos por otros motivos que no sean obligaciones contractuales (por ejemplo para cumplir con requisitos de contabilidad o requisitos para mantener registros de llamadas para interceptación legal) solo conservaremos los datos hasta que sea necesario y legal en cada caso.</p>
+
+
+<h3>8. Tus derechos de acceso, rectificación y supresión de datos personales</h3>
+<p>En virtud de la legislación vigente de protección de datos tienes los siguientes derechos con respecto a los datos personales:</p>
+
+<ul>
+  <li><i>Derecho de acceso – </i>Tienes derecho a obtener información de Phonealo acerca de cualquier dato personal que se procese sobre ti, así como a acceder a dichos datos, si corresponde.</li>
+  <li><i>Derecho a rectificación –</i> Tienes derecho a rectificar datos personales inexactos sobre ti.</li>
+  <li><i>Derecho a supresión ("el derecho a ser olvidado") – </i> Tienes derecho a suprimir cualquier dato sobre ti si estos datos ya no son necesarios para los fines para los que fueron recopilados. Es posible que existan obligaciones jurídicas que obliguen a Phonealo a mantener los datos, lo cual impedirá a Phonealo borrar dichos datos. En ese caso podemos bloquear los datos que estamos obligados a guardar para que no puedan utilizarse por ningún otro motivo que no sea el cumplimiento de los requisitos jurídicos; de igual modo también podemos convertir los datos en anónimos.</li>
+</ul>
+<p>
+Si deseas ejercer tus derechos con respecto a tus datos personales o si tienes alguna pregunta en relación con esta política de privacidad puedes ponerte en contacto con nosotros en la dirección contact@Phonealo.com.</p>
+
+<p>Última actualización: Agosto de 2018.</p>
+',
 
  
 
